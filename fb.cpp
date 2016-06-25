1e823be619aecacc1730283457d859b323f2f1ab
bao <chibaosen@gmail.com>
2016-06-25 11:36:13 +0700

    cool.

--- fb.cpp
+++ fb.cpp
@@ -1,7 +1,60 @@
-class Pix {
-};
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include "log.hpp"
 
-void draw(const Rect& r)
-{
-    Slt<fb>::get()(r.x, r.y, r.w. r.h);
+Fb::Fb() {
+    open();
+    setup();
+    fb = map();
+}
+
+void Fb::setup() {
+    const auto v = vinfo();
+    width = v.xres;
+    height = v.yres;
+}
+
+void Fb::open() {
+    fbfd = open("/dev/fb0", O_RDWR);
+
+    if (fbfd == -1)
+        die("Cannot open fb 0!");
+}
+
+char* Fb::map() {
+    size = finfo().smem_len;
+    return (char*)(mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0));
+}
+
+auto Fb::vinfo() {
+    fb_var_screeninfo vinfo;
+
+    if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo))
+        die("Can't read video screen information.");
+
+    return vinfo;
+}
+
+auto Fb::finfo() {
+    fb_fix_screeninfo finfo;
+
+    if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo))
+        die("Can't read fixed screen information.");
+
+    return finfo;
+}
+
+char& Fb::operator()(const unsigned int x, const unsigned int y) {
+    // TODO: Error check?
+    return fb[x + (y * widthres)];
+}
+
+Fb::~Fb() {
+    munmap(fb, fb_size);
+
+    if (close(fbfd) == -1)
+        die("Failed to close fb 0!");
 }
--- fb.hpp
+++ fb.hpp
@@ -1,89 +1,19 @@
 #pragma once
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <linux/fb.h>
-#include <sys/mman.h>
-#include <sys/ioctl.h>
-#include "log.hpp"
-
-template<int nrows, int ncols>
-class Vec<nrows, ncols> {
-};
-
 class Fb {
 public:
-    Fb()
-    {
-        // Setup the fb.
-        open_fb_file();
-        setup_screen_resolution();
-        fb = map();
-    }
-
-    char& operator()(const unsigned int x, const unsigned int y)
-    {
-        // TODO: Error check?
-        return fb[x + (y * widthres)];
-    }
-
-    ~Fb()
-    {
-        // Unmap fb from the address space.
-        munmap(fb, fb_size);
-
-        // Close the fb file descriptor.
-        if (close(fbfd) == -1)
-            die("Failed to close fb 0!");
-    }
-
+    Fb();
+    char& operator()(const unsigned int x, const unsigned int y);
+    ~Fb();
 private:
-    auto get_vinfo()
-    {
-        fb_var_screeninfo vinfo;
-
-        if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo))
-            die("Can't read video screen information.");
-
-        return vinfo;
-    }
-
-    auto getfinfo()
-    {
-        fb_fix_screeninfo finfo;
-
-        if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo)) {
-            die("Can't read fixed screen information.");
-        }
-
-        return finfo;
-    }
-
-    char* map()
-    {
-        fb_size = getfinfo().smem_len;
-        return static_cast<char*>(mmap(nullptr, fb_size, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0));
-    }
-
-    void open_fb_file()
-    {
-        fbfd = open("/dev/fb0", O_RDWR);
-
-        if (fbfd == -1)
-            die("Cannot open fb 0!");
-    }
-
-    void setup_screen_resolution()
-    {
-        const auto v = get_vinfo();
-        widthres = v.xres;
-        heightres = v.yres;
-    }
+    auto vinfo();
+    auto finfo();
+    char* map();
+    void open();
+    void setup();
 
     int fbfd;
-
-    unsigned int widthres, heightres;
-
-    char*  fb = nullptr;
-    size_t fb_size = 0;
+    unsigned int width, height;
+    char*  fb;
+    size_t size;
 };
