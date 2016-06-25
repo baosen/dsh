5d5f7d77a9cc2b2582035e8ce18abefabaec2722
bao <chibaosen@gmail.com>
2016-06-25 17:46:20 +0700

    asd

--- dshell.cpp
+++ dshell.cpp
@@ -1,50 +1,14 @@
-#include "freetype.hpp"
-#include "framebuffer.hpp"
+#include "ft.hpp"
+#include "fb.hpp"
 #include "slt.hpp"
-#include "keyboard.hpp"
-
-class Panel {
-public:
-// TODO: Drag.
-};
-
-// Different ways for windows to be laid out.
-class Layout {
-protected:
-    void place();
-};
-
-namespace {
-    void setup_dshell() {
-        init_freetype();
-        setup_framebuffer();
-    }
-    
-    void destroy_dshell() {
-        destroy_framebuffer();
-    }
-
-    void run_dshell() {
-        forever {
-            auto kbcode = get_keyboard_code();
-            switch (kbcode) {
-            }
-        }
-    }
-}
+#include "kb.hpp"
 
 int main()
 {
-    auto framebuffer = Singleton<Framebuffer>::get_instance();
-
     try {
-        setup_dshell();
-        run_dshell();
-        destroy_dshell();
     } catch(...) {
-        destroy_dshell();
         throw;
     }
 
-    return EXIT_SUCCESS;
+    return 1;
 }
--- obs.cpp
+++ obs.cpp
@@ -1,13 +1,3 @@
 #include <list>
-#include "keyboard.hpp"
+#include "kb.hpp"
 using namespace std;
-
-namespace {
-    typedef void (*f)();
-    list<f> keyboard_codes[MAX_KEYBOARD_CODES];
-}
-
-void handle(const int code, f f)
-{
-    keyboard_codes[code].push_back(f);
-}
