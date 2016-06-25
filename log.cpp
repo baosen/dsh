ce7af15639dff159e81333510e0a755f6735e096
bao <chibaosen@gmail.com>
2016-06-25 11:43:32 +0700

    poop

--- log.cpp
+++ log.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <cstdlib>
+using namespace std;
 
-void die(const char* error_message)
-{
-    std::cerr << error_message;
-    exit(EXIT_FAILURE);
+void die(const char* msg) {
+    cerr << msg;
+    exit(1);
 }
 
-void warn(const char *warning_message)
-{
-    // TODO: Yellowify!
-    std::cout << warning_message;
+void warn(const char *msg) {
+// TODO: Yellowify!
+    cout << msg;
 }
