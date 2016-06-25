79ef9b451cd5ac7c55949b12a68cf929f7b4a3d0
bao <chibaosen@gmail.com>
2016-06-25 11:40:48 +0700

    cool.

--- types.hpp
+++ types.hpp
@@ -1,2 +1,5 @@
 #pragma once
+#include <cstdint>
 typedef unsigned int uint;
+typedef uint32_t u32;
+typedef int32_t s32;
--- wgt.cpp
+++ wgt.cpp
@@ -1,11 +1,7 @@
 #include "types.hpp"
 #include "wnd.hpp"
-#include "framebuffer.hpp"
-using namespace dshell;
 
 namespace {
-    Fb fb;
-
     // TODO: Init.
     uint newx, newy;
 
@@ -18,12 +14,10 @@ namespace {
     }
 }
 
-namespace dshell {
-    // A widget is a desktop object a user can interact with.
-    class Wgt {
-    public:
-        Wgt() : w(0, 0, nextx(), nexty()) {}
-    private:
-        Wnd w;
-    };
-}
+// A widget is a desktop object a user can interact with.
+class Wgt {
+public:
+    Wgt() : w(0, 0, nextx(), nexty()) {}
+private:
+    Wnd w;
+};
