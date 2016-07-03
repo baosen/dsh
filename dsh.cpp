2923f8e5c62785eb8301b1f460a87a9c82841419
bao <chibaosen@gmail.com>
2016-07-03 21:07:25 +0700

    moved code.

--- dsh.cpp
+++ dsh.cpp
@@ -13,13 +13,6 @@ int main() {
         Wnd w2(Rect(Pos(200, 300), Res(100, 100)), Col(0, 0, 255));
         Wnd w3(Rect(Pos(300, 400), Res(100, 100)), Col(0, 0, 255));
         Wnd w4(Rect(Pos(400, 500), Res(100, 100)), Col(0, 0, 255));
-        //Fb fb;
-        //for (int i = 0; i < 5; i++) {
-        //    fb.fill(Rect(Pos(100+(i*100), 100), Res(100, 100)), Col(255, 0, 0));
-        //    fb.fill(Rect(Pos(100+(i*100), 200), Res(100, 100)), Col(0, 255, 0));
-        //    fb.fill(Rect(Pos(100+(i*100), 300), Res(100, 100)), Col(0, 0, 255));
-        //    fb.fill(Rect(Pos(100+(i*100), 400), Res(100, 100)), Col(0, 255, 255));
-        //}
     } catch (const Err& e) {
         cerr << "Error code: " << hex << scast<int>(e) << endl;
     } catch (const err& e) {
--- test.cpp
+++ test.cpp
@@ -1,10 +1,23 @@
 #include "ms.hpp"
 #include "types.hpp"
 
-int main() {
+void testms() {
     Ms m;
     forever {
         m.read();
     }
+}
+
+void testfb() {
+    Fb fb;
+    for (int i = 0; i < 5; i++) {
+        fb.fill(Rect(Pos(100+(i*100), 100), Res(100, 100)), Col(255, 0, 0));
+        fb.fill(Rect(Pos(100+(i*100), 200), Res(100, 100)), Col(0, 255, 0));
+        fb.fill(Rect(Pos(100+(i*100), 300), Res(100, 100)), Col(0, 0, 255));
+        fb.fill(Rect(Pos(100+(i*100), 400), Res(100, 100)), Col(0, 255, 255));
+    }
+}
+
+int main() {
     return 0;
 }
