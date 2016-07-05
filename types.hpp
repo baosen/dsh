392be971a31065326d07af3e391453382c1a1518
bao <chibaosen@gmail.com>
2016-07-05 22:44:16 +0700

    poop

--- types.hpp
+++ types.hpp
@@ -5,6 +5,7 @@ typedef unsigned int uint;
 typedef uint32_t u32;
 typedef int32_t s32;
 typedef uint8_t u8;
+typedef int8_t s8;
 #define scast static_cast
 #define rcast reinterpret_cast 
 #define ccast const_cast 
