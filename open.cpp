4f8b048eb1010bd29a44c96139bca546c5bcfa05
bao <chibaosen@gmail.com>
2016-07-08 07:01:31 +0200

    poop test code.

--- open.cpp
+++ open.cpp
@@ -1,7 +1,19 @@
 // The universal program to open, load and execute programs.
 #include <iostream>
+#include <cstdio>
 #include "path.hpp"
 using namespace std;
+
+vector<FILE*> open(const vector<string>& paths) {
+    vector<FILE*> v;
+    for (const auto& p : paths) {
+        const auto f = fopen(p.c_str(), "r");
+        if (!f)
+            continue;
+        v.push_back(f);
+    }
+    return v;
+}
 
 int main() {
     const auto path = getpath();
