250883cd6301e87859f045aeb4ef197ad5a7ba33
bao <chibaosen@gmail.com>
2017-02-11 02:14:50 +0100

    poop

---
+++ libdsh/scr.hpp
@@ -0,0 +1,31 @@
+class Ptr {
+    size_t size;
+    char* fb;
+public:
+    Ptr() {
+    }
+
+    Ptr(Scr& s) {
+        size = s.finfo().smem_len;
+        fb = scast<char*>(mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_SHARED, s.fd, 0));
+    }
+
+    char* operator*(const uint i) {
+    }
+
+    Ptr(const Ptr&) {
+    }
+
+    Ptr(const Ptr&&) {
+    }
+
+    Ptr operator=(const Ptr& p) {
+    }
+
+    ~Ptr() {
+        if (munmap(fb, size) == -1) {
+            perror("Error");
+            exit(errno);
+        }
+    }
+};
