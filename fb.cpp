07fef271b36fdb9f56b9e006360c62b0e62e2acd
bao <chibaosen@gmail.com>
2017-03-12 17:31:28 +0100

    Removed unused framebuffer code.

--- fb.cpp
+++ fb.cpp
@@ -30,27 +30,3 @@ u8& Fb::get8(const uint i) {
 u32& Fb::get32(const uint i) {
     return *(rcast<u32*>(fb)+i);
 }
-
-/*
-// Assign a pixel to (x, y) in the framebuffer.
-char& Fb::operator()(const Pos& p) {
-    const auto i = p.i(scr.w);
-    if (i >= scr.w * scr.h)
-        throw Fb::Err::OOR;
-    return fb[i];
-}
-
-// Fill a rectangle in the framebuffer with a color.
-void Fb::fill(const Rect& r, const Col& c) {
-    if (r.i() >= scr.w * scr.h)
-        throw Fb::Err::OOR;
-    r.fill(fb, c);
-}
-
-// Fill the entire screen with a color.
-void Fb::fill(const Col& c) {
-    const auto p = c.val(scr.roff, scr.goff, scr.boff);
-    for (size_t i = 0; i < scr.w*scr.h; ++i)
-        fb.i32(i) = p;
-}
-*/
