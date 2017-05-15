00e422fc573ecc5e7847b7760e465f7c72f58cdd
bao <chibaosen@gmail.com>
2017-05-15 18:25:36 +0200

    Typo.

--- wnd.hpp
+++ wnd.hpp
@@ -8,7 +8,7 @@ public:
         max,   // User wants to maximize window.
         min,   // User wants to minimize window.
         click, // User clicks somewhere on the window.
-        copy,  // User wants to copy an existing rectangular image to a window. Sends a raw or compressed rectangle image. User chooses driver.
+        copy,  // User wants to copy an existing rectangular image to a window. Sends a raw or compressed rectangular image. User chooses driver.
     };
     
     // Create null window.
