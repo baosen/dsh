303a0a17a9e9aac946af1b95fe2ff237feaa28aa
bao <chibaosen@gmail.com>
2017-05-17 11:46:59 +0200

    Docs.

--- res.cpp
+++ res.cpp
@@ -1,6 +1,10 @@
 #include "res.hpp"
 #include "err.hpp"
+
+// Create empty resolution.
 Res::Res() : w(0), h(0) {}
+
+// Create resolution of width w and height h.
 Res::Res(const uint w, const uint h) : w(w), h(h) {
     if (!w || !h)
         throw err("Resolution is zero!");
