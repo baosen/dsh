1033a4a073eaae3100e748c717764f55ef324748
bao <chibaosen@gmail.com>
2017-05-20 15:54:29 +0200

    Cleanup.

--- dsh.cpp
+++ dsh.cpp
@@ -1,7 +1,6 @@
 // dsh: Shell for desktops.
 #include <cstring>
 #include <cstdio>
-#include <linux/limits.h>
 #include "wnd.hpp"
 #include "log.hpp"
 #include "m.hpp"
@@ -165,8 +164,6 @@ static Pos topos(const M::Ev& e) {
 }
 
 namespace {
-    // Enable network capabilities.
-    bool neten = false;
     // Device numbers.
     int mousenum = -1, 
         evm      = -1;
@@ -181,9 +178,7 @@ static void init() {
     // Setup window.
     initw();
     // Setup network.
-    if (neten) {
-        //initnet();
-    }
+    //initnet();
 }
 
 // Server.
@@ -192,16 +187,15 @@ int main(const int argc, const char *argv[]) {
         // If argument provided.
         for (int i = 1; i < argc; ++i) {
             const auto s = argv[i];
-            if (!strcmp(s, "-net"))
-                neten = true;
+            // Set working directory for window files.
             if (!strcmp(s, "-dir")) {
                 char *path = nullptr;
                 if (sscanf(s+5, "%ms", &path) < 0) // m is part of POSIX 2008 (2013).
                     die("Invalid parameter.");
-                cout << path;
                 dsh::setcwd(path);
             }
-            if (!strcmp(s, "-m")) // Set "hacky" mouse.
+            // Set which "hacky" mouse to use.
+            if (!strcmp(s, "-m")) 
                 if (sscanf(s, "%d", &mousenum) < 0)
                     die("Invalid parameter.");
             if (!strcmp(s, "-e")) // Set event device. Automatically detects type.
