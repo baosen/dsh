bddaa8f40c41b901165dd6eaba6b401717422c04
bao <chibaosen@gmail.com>
2017-05-21 21:07:13 +0200

    TODO.

--- dpy.hpp
+++ dpy.hpp
@@ -6,6 +6,7 @@ class Dpy {
 public:
     // Open a display directory specified by the given path.
     Dpy(const char* path);
+    // TODO: Connect to a remote network display.
     ~Dpy();
 private:
     int fd;
