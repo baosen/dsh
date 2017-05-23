fda7d00c418a9257103707b785c8a69a6f9595a4
bao <chibaosen@gmail.com>
2017-05-23 13:53:10 +0200

    Display ideas.

--- dpy.cpp
+++ dpy.cpp
@@ -8,11 +8,17 @@ using namespace boost::filesystem;
 
 // Open a display directory specified by the given path.
 Dpy::Dpy(const string& path) {
+    // Create display directory containing all window and rectangle files.
     create_directory(path);
     dirpath = path;
 }
 
 // Disconnect from the display.
 Dpy::~Dpy() {
-    remove_all(dirpath);
+    // Delete all files in the display directory.
+    try {
+        remove_all(dirpath);
+    } catch (...) {
+        die("Display exception.");
+    }
 }
