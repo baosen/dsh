14a89c9a871481aa1563ce5410bd4bb7148db0d0
bao <chibaosen@gmail.com>
2017-05-23 22:18:10 +0200

    Removing directory idea.

--- dpy.cpp
+++ dpy.cpp
@@ -1,11 +1,9 @@
-#include <boost/filesystem.hpp>
 #include <sstream>
 #include "err.hpp"
 #include "log.hpp"
 #include "fio.hpp"
 #include "dpy.hpp"
 using namespace std;
-using namespace boost::filesystem;
 
 // Open a display directory specified by the given path.
 Dpy::Dpy(const string& path) {
@@ -14,17 +12,12 @@ Dpy::Dpy(const string& path) {
     // TODO: Parse and traverse path.
     stringstream ss;
     ss << path << "dpy" << i << '/';
-    create_directory(ss.str());
+    // TODO: Create file from FUSE driver.
     dirpath = ss.str();
     ++i;
 }
 
 // Disconnect from the display.
 Dpy::~Dpy() {
-    // Delete all files in the display directory.
-    try {
-        //remove_all(dirpath);
-    } catch (...) {
-        die("Display exception.");
-    }
+    die("TODO: Implement closing display.");
 }
