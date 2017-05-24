5a8007b12ac68d4c91bb19b72b551fed2ad31dbf
bao <chibaosen@gmail.com>
2017-05-24 13:08:23 +0200

    Cleanup.

--- dpyfs.c
+++ dpyfs.c
@@ -1,11 +1,7 @@
 #define FUSE_USE_VERSION 26
 #include <fuse.h>
-#include <stdio.h>
 #include <string.h>
 #include <errno.h>
-#include <fcntl.h>
-#include <stddef.h>
-#include <assert.h>
 
 static const char *filename = "dpy0";
 static const char *contents = "Display 0";
