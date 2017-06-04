c5477f0cd93ee5cc7d0980993da4f80e207c0010
bao <chibaosen@gmail.com>
2017-06-04 14:17:03 +0200

    Refactoring.

--- dshfs.cpp
+++ dshfs.cpp
@@ -39,190 +39,194 @@ template<class F> auto doifentry(const char *path, F f) {
     return -ENOENT;
 }
 
-// Initialize desktop shell file system.
-static void *dsh_init(struct fuse_conn_info *conn) noexcept {
-    return nullptr;
-}
-
-// Create shell file.
-static int dsh_create(const char *path, mode_t mode, struct fuse_file_info *fi) {
-    // Caller can only create files of type dpy* and wnd*.
-    return filedo(path, [](const char *p) {
-        // Create new display.
-        ents.emplace_back(File(p));
-        return 0;
-    }, [](const char *p) {
-        // Create new window.
-        ents.emplace_back(File(p));
-        return 0;
-    }, [](const char *p) {
-        // Create new keyboard.
-        ents.emplace_back(File(p));
-        return 0;
-    });
-    return -EINVAL; // Invalid path.
-}
-
-// Get file attributes of a file in the shell file system.
-static int dsh_getattr(const char *path, struct stat *buf) noexcept {
-    // Prepare stat-buffer.
-    zero(*buf);
-    // If caller wants to check the attributes of the backslash directory.
-    if (!strcmp(path, "/")) {
-        buf->st_mode = S_IFDIR | 0755; // Directory.
-        buf->st_nlink = 0;             // Number of hardlinks that points to this file that exists in the file system.
-        return 0;
-    } 
-    return doifentry(path, [&]() {
-        buf->st_mode  = S_IFREG | 0444; // mode bits.
-        buf->st_nlink = 0;
-        buf->st_size  = 0;
-        return 0;
-    });
-}
-
-// Read directory tree.
-static int dsh_readdir(const char *path, void *buf, fuse_fill_dir_t fill, off_t offset, struct fuse_file_info *fi) {
-    // Fill recursively.
-    for (const auto& e : ents)
-        // Build the file entries in the buffer.
-        fill(buf, e.name.c_str(), nullptr, 0);
-    return 0;
-}
-
-// Does the file exist in one of the entries?
-static bool exists(const char *name) {
-    for (const auto& e : ents)
-        if (!strcmp(name, e.name.c_str()))
-            return true;
-    return false;
-}
-
-// Open the desktop shell file system.
-static int dsh_open(const char *path, struct fuse_file_info *fi) noexcept {
-    return filedo(path, [&](const char *p) {
-        if (!exists(p))
-            return -ENOENT;
-    }, [&](const char *p) {
-        if (!exists(p))
-            return -ENOENT; // No entry found.
-    }, [](const char *p) {
-        if (!exists(p))
-            return -ENOENT;
-    });
-    return 0;
-}
-
-// Read file contents.
-static int dsh_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) noexcept {
-    return doifentry(path, [&]() {
-        return filedo(path, [](const char *p) { // Display.
-            // TODO.
+namespace fs {
+    // Initialize desktop shell file system.
+    void *init(struct fuse_conn_info *conn) noexcept {
+        return nullptr;
+    }
+    
+    // Create shell file.
+    int create(const char *path, mode_t mode, struct fuse_file_info *fi) {
+        // Caller can only create files of type dpy* and wnd*.
+        return filedo(path, [](const char *p) {
+            // Create new display.
+            ents.emplace_back(File(p));
             return 0;
-        }, [](const char *p) {                  // Window.
-            // TODO.
+        }, [](const char *p) {
+            // Create new window.
+            ents.emplace_back(File(p));
             return 0;
-        }, [](const char *p) {                  // Keyboard.
-            // Read key code.
-            return kb.get();
+        }, [](const char *p) {
+            // Create new keyboard.
+            ents.emplace_back(File(p));
+            return 0;
         });
-    });
-}
-
-// Write to display. Returns exactly the number of bytes written except on error.
-static int dsh_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) noexcept {
-    return doifentry(path, [&]() {
-        return filedo(path, [](const char *p) { // Display.
-            // TODO.
+        return -EINVAL; // Invalid path.
+    }
+    
+    // Get file attributes of a file in the shell file system.
+    int getattr(const char *path, struct stat *buf) noexcept {
+        // Prepare stat-buffer.
+        zero(*buf);
+        // If caller wants to check the attributes of the backslash directory.
+        if (!strcmp(path, "/")) {
+            buf->st_mode = S_IFDIR | 0755; // Directory.
+            buf->st_nlink = 0;             // Number of hardlinks that points to this file that exists in the file system.
             return 0;
-        }, [](const char *p) {                  // Window.
-            // TODO.
+        } 
+        return doifentry(path, [&]() {
+            buf->st_mode  = S_IFREG | 0444; // mode bits.
+            buf->st_nlink = 0;
+            buf->st_size  = 0;
             return 0;
-        }, [](const char *p) {                  // Keyboard.
-            // Keyboard is read-only.
-            return -EPERM; // Operation not permitted.
         });
-    });
-}
-
-// Control files in shell file system.
-static int dsh_ioctl(const char *path, int cmd, void *arg, struct fuse_file_info *fi, unsigned int flags, void *data) noexcept {
-    return doifentry(path, [&]() {
-        return filedo(path, [&](const char *p) { // Display.
-            return dpycmd(cmd);
-        }, [&](const char *p) {                  // Window.
-            return wndcmd(cmd);
-        }, [&](const char *p) {                  // Keyboard.
-            // No commands for keyboards.
-            return -EINVAL;
+    }
+    
+    // Read directory tree.
+    int readdir(const char *path, void *buf, fuse_fill_dir_t fill, off_t offset, struct fuse_file_info *fi) {
+        // Fill recursively.
+        for (const auto& e : ents)
+            // Build the file entries in the buffer.
+            fill(buf, e.name.c_str(), nullptr, 0);
+        return 0;
+    }
+    
+    // Does the file exist in one of the entries?
+    bool exists(const char *name) {
+        for (const auto& e : ents)
+            if (!strcmp(name, e.name.c_str()))
+                return true;
+        return false;
+    }
+    
+    // Open the desktop shell file system.
+    int open(const char *path, struct fuse_file_info *fi) noexcept {
+        return filedo(path, [&](const char *p) {
+            if (!exists(p))
+                return -ENOENT;
+        }, [&](const char *p) {
+            if (!exists(p))
+                return -ENOENT; // No entry found.
+        }, [](const char *p) {
+            if (!exists(p))
+                return -ENOENT;
         });
-    });
-}
-
-// Make shell file node. Gets called for creation of all non-directory, non-symbolic link nodes.
-static int dsh_mknod(const char *path, mode_t mode, dev_t dev) {
-    return dsh_create(path, mode, nullptr);
-}
-
-// Create standard "this" and "parent" links.
-static void mklns() {
-    ents.emplace_back(File("."));
-    ents.emplace_back(File(".."));
-}
-
-// Setup and initialize displays, and make the files pointing to them.
-static void mkdpys() {
-    // Initialize graphical output.
-    dpyinit();
-    static uint i = 0; // Current index of display.
-    stringstream ss;
-    ss << "dpy" << i;
-    ents.emplace_back(File(ss.str()));
-}
-
-// Setup mouse and make mouse files.
-static void mkm() {
-    // Initialize mouse.
-    initm();
-    // Make mouse files.
-    static uint i = 0;
-    stringstream ss;
-    ss << "m" << i;
-    ents.emplace_back(File(ss.str()));
-}
-
-// Setup keyboard.
-static void mkkb() {
-    // Initialize keyboard.
-    initkb();
-    // Insert it into filesystem.
-    static uint i = 0; // Current index of keyboard.
-    stringstream ss;
-    ss << "kb" << i;
-    ents.emplace_back(File(ss.str()));
-}
-
-static void cleanup() {
-    // TODO: Cleanup keyboard, sound, mouse.
-    dpydeinit();
+        return 0;
+    }
+    
+    // Read file contents.
+    int read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) noexcept {
+        return doifentry(path, [&]() {
+            return filedo(path, [](const char *p) { // Display.
+                // TODO.
+                return 0;
+            }, [](const char *p) {                  // Window.
+                // TODO.
+                return 0;
+            }, [](const char *p) {                  // Keyboard.
+                // Read key code.
+                return kb.get();
+            });
+        });
+    }
+    
+    // Write to display. Returns exactly the number of bytes written except on error.
+    int write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) noexcept {
+        return doifentry(path, [&]() {
+            return filedo(path, [](const char *p) { // Display.
+                // TODO.
+                return 0;
+            }, [](const char *p) {                  // Window.
+                // TODO.
+                return 0;
+            }, [](const char *p) {                  // Keyboard.
+                // Keyboard is read-only.
+                return -EPERM; // Operation not permitted.
+            });
+        });
+    }
+    
+    // Control files in shell file system.
+    int ioctl(const char *path, int cmd, void *arg, struct fuse_file_info *fi, unsigned int flags, void *data) noexcept {
+        return doifentry(path, [&]() {
+            return filedo(path, [&](const char *p) { // Display.
+                return dpycmd(cmd);
+            }, [&](const char *p) {                  // Window.
+                return wndcmd(cmd);
+            }, [&](const char *p) {                  // Keyboard.
+                // No commands for keyboards.
+                return -EINVAL;
+            });
+        });
+    }
+    
+    // Make shell file node. Gets called for creation of all non-directory, non-symbolic link nodes.
+    int mknod(const char *path, mode_t mode, dev_t dev) {
+        return create(path, mode, nullptr);
+    }
+    
+    // Create standard "this" and "parent" links.
+    void mklns() {
+        ents.emplace_back(File("."));
+        ents.emplace_back(File(".."));
+    }
+    
+    // Setup and initialize displays, and make the files pointing to them.
+    void mkdpys() {
+        // Initialize graphical output.
+        dpyinit();
+        static uint i = 0; // Current index of display.
+        stringstream ss;
+        ss << "dpy" << i;
+        ents.emplace_back(File(ss.str()));
+    }
+    
+    // Setup mouse and make mouse files.
+    void mkm() {
+        // Initialize mouse.
+        initm();
+        // Make mouse files.
+        static uint i = 0;
+        stringstream ss;
+        ss << "m" << i;
+        ents.emplace_back(File(ss.str()));
+    }
+    
+    // Setup keyboard.
+    void mkkb() {
+        // Initialize keyboard.
+        initkb();
+        // Insert it into filesystem.
+        static uint i = 0; // Current index of keyboard.
+        stringstream ss;
+        ss << "kb" << i;
+        ents.emplace_back(File(ss.str()));
+    }
+    
+    void cleanup() {
+        // TODO: Cleanup keyboard, sound, mouse.
+        dpydeinit();
+    }
 }
 
 // File system driver for displays.
 int main(int argc, char *argv[]) {
+    using namespace fs;
+
     // Setup file system operations.
     static fuse_operations ops = {0};
     // Creation.
-    ops.init    = dsh_init;    // Initialize file system.
-    ops.create  = dsh_create;  // Create file.
-    ops.mknod   = dsh_mknod;   // Make file node.
+    ops.init    = init;      // Initialize file system.
+    ops.create  = create;    // Create file.
+    ops.mknod   = fs::mknod; // Make file node.
     // I/O.
-    ops.getattr = dsh_getattr; // Get attributes.
-    ops.readdir = dsh_readdir; // Read directory.
-    ops.open    = dsh_open;    // Open display to be worked upon.
-    ops.read    = dsh_read;    // Read display's contents.
-    ops.write   = dsh_write;   // Write to the display's contents.
+    ops.getattr = getattr; // Get attributes.
+    ops.readdir = readdir; // Read directory.
+    ops.open    = open;    // Open display to be worked upon.
+    ops.read    = read;    // Read display's contents.
+    ops.write   = write;   // Write to the display's contents.
     // Control.
-    ops.ioctl   = dsh_ioctl;   // Control display.
+    ops.ioctl   = ioctl;   // Control display.
 
     // Start our engines!
     try {
