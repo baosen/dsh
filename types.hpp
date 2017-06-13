83117b1366ac3f5e9377e047dcde833378635073
bao <chibaosen@gmail.com>
2017-06-14 01:13:01 +0200

    Cleanup.

--- types.hpp
+++ types.hpp
@@ -7,8 +7,8 @@ typedef uint32_t       u32;
 typedef int32_t        s32;
 typedef uint8_t        u8;
 typedef int8_t         s8;
-#define scast static_cast
-#define rcast reinterpret_cast 
-#define ccast const_cast 
-#define forever for (;;)
-using err = std::runtime_error;
+#define scast          static_cast
+#define rcast          reinterpret_cast 
+#define ccast          const_cast 
+#define forever        for (;;)
+using err =            std::runtime_error;
