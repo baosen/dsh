9b516938ccf30c480a8b67dbe7b1b54404bcecc2
bao <chibaosen@gmail.com>
2017-06-28 01:14:11 +0200

    Simplified.

--- drm.cpp
+++ drm.cpp
@@ -1,20 +1,27 @@
 #include <xf86drm.h>
+#include "types.hpp"
 
 namespace {
     int fd = -1;
+
+    // DRM instance.
+    struct Drminst {
+        drmModeRes       *resources;
+        drmModeConnector *connector;
+        drmModeEncoder   *encoder;
+    }
 }
 
 // Initialize Direct-Rendering Manager.
 void drm::init(const char *path) {
-    drmModeRes       *resources = nullptr;
-    drmModeConnector *connector = nullptr;
-    drmModeEncoder   *encoder   = nullptr;
-
+    Drminst inst;
+    zero(inst);
     if (fd = open(path, O_RDWR) == -1)
         throw errno; // TODO!
 }
 
 // Deinitialize the direct rendering manager.
 void drm::deinit() {
-    close(fd);
+    if (close(fd) == -1)
+        die("Failed to close DRM!");
 }
