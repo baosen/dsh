6c63bf607dc4dc3b847dc48553a1072303d79a11
bao <chibaosen@gmail.com>
2017-06-28 20:56:32 +0200

    Ideas.

--- dsys.hpp
+++ dsys.hpp
@@ -4,8 +4,18 @@
 
 // Display system.
 namespace dsys {
+	// Initialize display system.
     void init();
-    void write(const char* name, const char *buf, const off_t i, const size_t size) noexcept;
+	// Read from display.
     void read(const char* name, char *buf, const off_t i, const size_t size) noexcept;
+	// Write to display.
+    void write(const char* name, const char *buf, const off_t i, const size_t size) noexcept;
+	// De-nitialize display system.
     void deinit();
+
+	// Bit depths of color components.
+	uint red,   // Bit depth of the red component.
+		 green, // Bit depth of the green component.
+		 blue,  // Bit depth of the blue component.
+		 alpha; // Bit depth of the alpha component.
 }
