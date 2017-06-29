1cae2939722432cab1dbb5a85bb89abacb1103ac
bao <chibaosen@gmail.com>
2017-06-29 12:57:02 +0200

    Docs.

--- dshfs.cpp
+++ dshfs.cpp
@@ -9,17 +9,18 @@ int main(int argc, char *argv[]) {
 
     // Setup file system operations.
     static fuse_operations ops = {0};
-    // Creation.
+
+    // Creation-functions:
     ops.init    = init;      // Initialize file system.
     ops.create  = create;    // Create file.
     ops.mknod   = fs::mknod; // Make file node.
-    // I/O.
+    // I/O-functions:
     ops.getattr = getattr; // Get attributes.
     ops.readdir = readdir; // Read directory.
     ops.open    = open;    // Open display to be worked upon.
     ops.read    = read;    // Read display's contents.
     ops.write   = write;   // Write to the display's contents.
-    // Control.
+    // Control-functions:
     ops.ioctl   = ioctl;   // Control display.
 
     // Start our engines!
@@ -30,16 +31,16 @@ int main(int argc, char *argv[]) {
         // Drive user-space file system.
         ret = fuse_main(argc, argv, &ops, nullptr);
     } catch (...) {
-        // TODO! Show more informed error messages.
-        error("Exception caught!");
+        // TODO: Show more informed error messages.
+        error("An exception was caught!");
         ret = EXIT_FAILURE;
     }
     try {
-        // Cleanup shell system.
+        // Cleanup the shell file system.
         cleanup();
     } catch (...) {
-        // TODO! Show more informed error messages.
-        error("Exception caught!");
+        // TODO: Show more informed error messages.
+        error("An exception caught!");
         ret = EXIT_FAILURE;
     }
     return ret;
