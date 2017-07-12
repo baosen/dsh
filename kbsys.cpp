3ebe58d175dbc012f832495096a19644366d9226
bao <chibaosen@gmail.com>
2017-07-12 12:16:55 +0200

    Ideas for keyboard system.

--- kbsys.cpp
+++ kbsys.cpp
@@ -1,18 +1,64 @@
+#include <thread>
+#include <deque>
+#include <mutex>
 #include "kbsys.hpp"
+using namespace std;
 
-// Keyboard system for file system.
-namespace kbsys {
-    Kb kb;
+// TODO: Keyboard subsystem for file system.
+namespace {
+    Kb                 kb;            // A handle to the keyboard itself.
+
+    thread             k1,            // Handling keyboard file event1.
+                       k2;            // Handling keyboard file event2.
+    bool               kexit = false; // Set to exit and stop the keyboard threads.
+
+    deque<input_event> ievs;          // Queue of keyboard events.
+    mutex              m;             // Lock to use to make the threads share the queue together.
+}
+
+// Append new keyboard event to the queue.
+static void append(const input_event& e)
+{
+    // TODO: LOCK!
+    m.lock();
+
+    ievs.push_back(e);
+}
+
+// Get next keyboard event.
+input_event get()
+{
+    // TODO: LOCK!
+    m.lock();
+    
+    // Get next keyboard event.
+    const auto e = ievs.front();
+    ievs.pop_front();
+    return e;
 }
 
 // Setup and initialize keyboard.
-void kbsys::init() {
+void kbsys::init() 
+{
     // Open keyboard file.
     kb.open();
+
+    // Setup keyboard threads.
+    k1 = thread([](){
+        while (!kexit) // TODO: Potential race that can be ignored.
+            append(kb.rd1());
+    });
+    k2 = thread([](){
+        while (!kexit) // TODO: Potential race that can be ignored.
+            append(kb.rd2());
+    });
 }
 
 // Cleanup keyboard subsystem.
-void kbsys::deinit() {
+void kbsys::deinit() 
+{
+    // Exit keyboard threads.
+    kexit = true;
     // Close keyboard file.
     kb.close();
 }
--- kbsys.hpp
+++ kbsys.hpp
@@ -3,10 +3,11 @@
 
 // Keyboard subsystem for file system.
 namespace kbsys {
-    extern Kb kb;
-
     // Initialize keyboard subsystem.
     void init();
     // Deinitialize keyboard subsystem.
     void deinit();
+
+    // Get next keyboard event.
+    input_event get();
 }
