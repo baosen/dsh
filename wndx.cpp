e2508c3c9e656ada186b82cfa58a92377a3269df
bao <chibaosen@gmail.com>
2017-07-22 16:45:09 +0200

    Docs.

--- wndx.cpp
+++ wndx.cpp
@@ -8,14 +8,17 @@ using namespace std;
 // Unsigned int x coordinate file for window.
 // TODO: Make /wnd/0/xc for x ascii.
 
+// Create window file x.
 wndx::wndx(const shared_ptr<wndd>& p)
     : parent(p), file("x")
 {}
 
+// Destroy window file x.
 wndx::wndx()
     : file("x")
 {}
 
+// Read window x coordinate.
 int wndx::read(const char  *path,   // File path.
                char        *buf,    // Buffer to read from.
                const off_t  i,      // Offset.
@@ -33,6 +36,7 @@ int wndx::read(const char  *path,   // File path.
     return nbytes;
 }
 
+// Write window x coordinate.
 int wndx::write(const char  *path,   // File path.
                 const char  *buf,    // Buffer to write from.
                 const off_t  i,      // Index.
