d28aea3426e8c045065d9b41abd9c55c18a40053
bao <chibaosen@gmail.com>
2017-08-01 16:14:04 +0200

    wndxc implemented but not tested.

--- wndx.cpp
+++ wndx.cpp
@@ -6,11 +6,11 @@
 using namespace std;
 
 // Unsigned int x coordinate file for window.
-// TODO: Make /wnd/0/xc for x ascii.
 
 // Create window file x.
 wndx::wndx(const std::string& p)
-    : file("x"), pname(p)
+    : file("x"), 
+      pname(p)   // Parent directory name.
 {}
 
 // Read window x coordinate.
@@ -21,8 +21,8 @@ int wndx::read(char        *buf,    // Buffer to read from.
     uint x;
 
     // TODO: Change to divisible check?
-    if (nbytes > sizeof(x) || i + (nbytes-1) >= sizeof(x))
-        return -EINVAL;
+    if (nbytes > sizeof(x) || i + (nbytes - 1) >= sizeof(x))
+        return -EINVAL; // Invalid parameter.
 
     x = wsys::getx(pname.c_str());
     memcpy(buf + i, &x, nbytes);
@@ -32,16 +32,16 @@ int wndx::read(char        *buf,    // Buffer to read from.
 
 // Write window x coordinate.
 int wndx::write(const char  *buf,    // Buffer to write from.
-                const off_t  i,      // Index.
+                const off_t  i,      // Offset.
                 const size_t nbytes) // Number of bytes to write.
 {
     uint x = 0;
 
     // TODO: Change to divisible check?
-    if (nbytes > sizeof(x) || i + (nbytes-1) >= sizeof(x))
-        return -EINVAL;
+    if (nbytes > sizeof(x) || i + (nbytes - 1) >= sizeof(x))
+        return -EINVAL; // Invalid parameter.
 
-    memcpy(rcast<char*>(&x) + i, buf, nbytes);
+    memcpy(rcast<byte*>(&x) + i, buf, nbytes);
     wsys::movex(pname.c_str(), x);
 
     return nbytes;
