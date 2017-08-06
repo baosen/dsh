213966faefeeac3bb0442186c32198e2d1720c36
bao <chibaosen@gmail.com>
2017-08-06 14:34:17 +0200

    Docs.

--- dshfs.cpp
+++ dshfs.cpp
@@ -14,25 +14,28 @@ int main(const int   argc,   // Number of arguments.
 
     // Creation-functions:
     o.init    = init;      // Initialize the shell file system.
-    o.create  = create;    // Create a file.
-    o.mknod   = fs::mknod; // Make a file node.
+    o.create  = create;    // Create a new file.
+    o.mknod   = fs::mknod; // Make a new file node. Almost the same as create().
+
     // I/O-functions:
-    o.getattr = getattr; // Get file attributes.
-    o.readdir = readdir; // Read a directory.
-    o.open    = open;    // Open file
-    o.read    = read;    // Read file contents.
-    o.write   = write;   // Write to the contents of a file.
+    o.getattr = getattr;   // Get file attributes.
+    o.readdir = readdir;   // Read a directory.
+    o.open    = open;      // Open a file
+    o.read    = read;      // Read the file contents.
+    o.write   = write;     // Write to the contents of a file.
+
     // Control-functions:
-    o.ioctl   = ioctl;   // Control device.
+    o.ioctl   = ioctl;     // Control device.
 
     // Start our engines!
     try {
         // Drive user-space file system.
         return fuse_main(argc, argv, &o, nullptr);
     } catch (const err& e) {
+        // Something wrong happened...
         error(e.what());
     } catch (...) {
-        error("An exception from the file system was caught!");
+        error("An exception from the shell file system was caught!");
     }
 
     // Cleanup the shell file system.
@@ -41,7 +44,8 @@ int main(const int   argc,   // Number of arguments.
     } catch (const err& e) {
         error(e.what());
     } catch (...) {
-        error("An exception on cleanup was caught!");
+        error("An exception on trying to cleanup the shell file system was caught!");
     }
+
     return EXIT_FAILURE;
 }
